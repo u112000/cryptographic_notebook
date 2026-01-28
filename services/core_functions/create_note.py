605121788cc88491c28b67657e625a1625b6400c
u112000 <oghenakogieidornigie@gmail.com>
2026-01-28 05:42:28 +0100

    Delete services directory

--- services/core_functions/create_note.py
+++
@@ -1,9 +0,0 @@
-#! python3
-
-def create_note():
-    from ...core.admin_database import DatabaseMain
-    from ...cli.core_functions_display.create_note_display import create_parameters
-    create_parameters_data: tuple = create_parameters()
-    db = DatabaseMain()
-    db.main_inserter(create_parameters_data)
-
